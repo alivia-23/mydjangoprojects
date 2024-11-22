0939275033010e4b98283b421de1a12c8c12d6c8
alivia-23 <guin.al@husky.neu.edu>
2024-11-22 12:20:02 -0800

    create api

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_user
+from .views import get_user, create_user
 
 urlpatterns = [
-    path('user/', get_user, name='get_user')
+    path('user/', get_user, name='get_user'),
+    path('user/create/', create_user, name='create_user')
 ]
